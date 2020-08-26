323fdc2e9312425ac054d31ef6c3947e69878fbe
captbilard <bilardosinulu@gmail.com>
2020-08-26 17:53:33 +0100

    Add a functionality page that allows staff to send an email to all existing users, using Django's console.EmailBackend to send the emails.

--- savests/admin_ui/admin.py
+++ savests/admin_ui/admin.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from django.utils.html import format_html
 from django.contrib import messages
 from django.utils.translation import ngettext
+from django.urls import path
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
 
 # Register your models here.
 admin.site.site_header = 'SaVests Admin Dashboard'
@@ -12,7 +15,32 @@ admin.site.site_header = 'SaVests Admin Dashboard'
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name', 'last_name', 'email','is_active', 'is_staff')
     list_filter = ('date_joined','last_login', 'is_active', 'is_staff')
+    change_list_template = 'admin/admin_ui/admin_ui_change_list.html'
     actions = ['make_active', 'make_inactive', 'make_staff', 'remove_staff']
+
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('sendmail/', self.send_mail_view)
+        ]
+        return my_urls + urls
+
+    def send_mail_view(self, request):
+        users = self.model.objects.all()
+        sender = request.POST['sender']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        recipient = [user.email for user in users]
+        send_mail(
+            subject,
+            message,
+            sender,
+            recipient,
+            fail_silently=False,
+        )
+        return HttpResponseRedirect('../')
+        
 
     def make_active(self, request, queryset):
         updated = queryset.update(is_active = True)
@@ -45,6 +73,8 @@ class UserAdmin(admin.ModelAdmin):
             '%d users are no longer staffs',
             updated
         ) % updated, messages.SUCCESS)
+
+    
     
     make_active.short_description = "Make selected users active"
     make_inactive.short_description = "Make selected users inactive"
